b3ed835b31d26b064c5de27f230b4cd8fdcb12b4
Tariq <f158185@nu.edu.pk>
2023-09-25 03:04:30 +0500

    initial commit

---
+++ apps/admin-eshop/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'inspirelogix-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass'],
+})
+export class AppComponent {
+  title = 'admin-eshop';
+}
