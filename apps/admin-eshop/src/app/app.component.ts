a215c018ae0c2c7e05876ea04df793c9470fca8b
Tariq Muhammad <tariqmuhammad.appelit@nextbridge.com>
2023-10-05 00:42:42 +0500

    structured styles files

--- apps/admin-eshop/src/app/app.component.ts
+++ apps/admin-eshop/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'inspirelogix-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'admin-eshop';
